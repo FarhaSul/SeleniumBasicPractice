7198dbe960aca4a7d85555bf64d1ab134fbe8492
farha <farha@DESKTOP-5TH367B>
2024-07-22 17:21:21 -0400

    first commit

---
+++ src/Sl/prg.java
@@ -0,0 +1,16 @@
+package Sl;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class prg {
+
+	public static void main(String[] args) {
+		System.setProperty("WebDriver.Chrome.driver", "chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+	driver.get("https://www.facebook.com/");
+
+	}
+
+}
